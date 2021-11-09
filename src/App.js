3abf064ff179447bb61c8b11d7073a3dab40ada2
PasqualCarstens <pasqualcarstens@gmail.com>
2021-11-09 16:01:35 +1100

    Worked on the search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div clssName="container">
+        <header className="App-header">Dictionary</header>
+        <Dictionary />
+        <footer className="App-footer">Coded by Pasqual Carstens</footer>
+      </div>
     </div>
   );
 }
