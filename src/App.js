a8aee97ad85a584cf7b9758aa91090e147125a07
PasqualCarstens <pasqualcarstens@gmail.com>
2021-11-09 17:53:52 +1100

    Added GitHub link and icon

--- src/App.js
+++ src/App.js
@@ -7,7 +7,17 @@ export default function App() {
       <div clssName="container">
         <header className="App-header">Dictionary</header>
         <Dictionary />
-        <footer className="App-footer">Coded by Pasqual Carstens</footer>
+        <footer className="App-footer">
+          <i className="fab fa-github github-icon"></i>{" "}
+          <a
+            href="https://github.com/PasqualCarstens/react-dictionary-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Pasqual Carstens
+        </footer>
       </div>
     </div>
   );
